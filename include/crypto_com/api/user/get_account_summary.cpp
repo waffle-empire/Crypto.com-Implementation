cae229c3040da3bf6a487ba154c5020b2da8759b
Joren-vanGoethem <jorenvangoethem@hotmail.com>
2022-02-13 13:44:26 +0100

    websocket endpoint: get account summary

---
+++ include/crypto_com/api/user/get_account_summary.cpp
@@ -0,0 +1,24 @@
+#include "../user.hpp"
+#include "../../util.hpp"
+
+namespace crypto_com
+{
+    void UserAPI::get_account_summary(std::string currency = "")
+    {
+        nlohmann::json pl = nlohmann::json{
+            { "method", "private/get-account-summary" },
+            { "nonce", util::create_nonce() }
+        };
+
+        pl["params"] = nlohmann::json::object();
+
+        if (currency.empty())
+            pl["params"] = nlohmann::json::object();
+        else
+            pl["params"] = nlohmann::json{ { "currency", currency } };
+
+        util::sign_payload(pl, this->m_key, this->m_secret);
+
+        m_ws_client->send(pl);
+    }
+} // namespace crypto_com
