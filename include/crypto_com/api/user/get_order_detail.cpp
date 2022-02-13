7ea5ed780dacd21c0e43161713804e49967bcfbd
Joren-vanGoethem <jorenvangoethem@hotmail.com>
2022-02-13 13:44:40 +0100

    websocket endpoint: get order detail

---
+++ include/crypto_com/api/user/get_order_detail.cpp
@@ -0,0 +1,22 @@
+#include "../user.hpp"
+#include "../../util.hpp"
+
+namespace crypto_com
+{
+    void UserAPI::get_order_detail(std::string order_id)
+    {
+        nlohmann::json pl = nlohmann::json{
+            { "method", "private/get-order-detail" },
+            { "nonce", util::create_nonce() },
+            {
+                "params", {
+                    {"order_id", order_id}
+                }
+            }
+        };
+        
+        util::sign_payload(pl, this->m_key, this->m_secret);
+
+        m_ws_client->send(pl);
+    }
+} // namespace crypto_com
