d3a9878214d10d1515cc324630ca6c8398b84fc5
Joren-vanGoethem <jorenvangoethem@hotmail.com>
2022-02-13 13:44:53 +0100

    websocket endpoint: get trades

---
+++ include/crypto_com/api/user/get_trades.cpp
@@ -0,0 +1,32 @@
+#include "../user.hpp"
+#include "../../util.hpp"
+
+namespace crypto_com
+{
+    void UserAPI::get_trades(std::string instrument_name = "", long start_ts = 0, long end_ts = 0,
+                int page_size = 0, int page = 0)
+    {
+        nlohmann::json pl = nlohmann::json{
+            { "method", "private/get-trades" },
+            { "nonce", util::create_nonce() }
+        };
+
+        pl["params"] = nlohmann::json::object();
+
+        if (!instrument_name.empty())
+            pl["params"]["instrument_name"] = instrument_name;
+
+        if (start_ts)
+            pl["params"]["start_ts"] = start_ts;
+        if (end_ts)
+            pl["params"]["end_ts"] = end_ts;
+        if (page_size)
+            pl["params"]["page_size"] = page_size;
+        if (page)
+            pl["params"]["page"] = page;
+
+        util::sign_payload(pl, this->m_key, this->m_secret);
+
+        m_ws_client->send(pl);
+    }
+} // namespace crypto_com
